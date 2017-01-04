8f4a6543e1f5d9c7377a8b4ca7e6907cc3651a79
cawi123 <michael_200@hotmail.es>
2017-01-03 21:22:36 -0500

    realizacion del deber aristas par e impar

--- n lados par impar.py
+++ n lados par impar.py
@@ -3,18 +3,17 @@ t=turtle.Pen()
 
 
 n=int(input("ingrese un numero de lados "))
-a=n/2
-b=180/a
-c=180-b
+
 #saer si n es par o inpar 
 if(n%2==0):
     #numero par de n lados 
     for x in range(n):
-#va a delante hasta cuantas veces debe girar a la derecha
+    #va a delante hasta cuantas veces debe girar a la derecha
         t.forward(108)
-        t.left(c)
-        t.left(c)
-        t.left(c)
+        t.left((n-3)*180/n)
+        t.left((n-3)*180/n)
+
+        
 else:
     for x in range(n):
 #va a delante hasta cuantas veces debe girar a la derecha
@@ -27,3 +26,4 @@ turtle.getscreen()._root.mainloop()
 
 
 #365 divido para el numero de lados
+
